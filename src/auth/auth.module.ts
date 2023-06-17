c839ed0e32670d6500a2c5b29c3de415370ea4a9
Daniela Liendo <daniela@ualabee.com>
2023-06-18 00:55:00 +0200

    jwt and config modules were added

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,19 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])	],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' }
+    })
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
