927c8dbdd59a16423c09290f47ee4ae38ca2eb5e
Daniela Liendo <daniela@ualabee.com>
2023-06-18 00:52:20 +0200

    user entirity props were updated

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -2,12 +2,12 @@ import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 
 @Schema()
 export class User {
-  // _id: string;
+  _id?: string;
   @Prop({ required: true, unique: true })
   email: string;
-  @Prop({ required: true, minlength: 6 })
-  password: string;
-  @Prop({ required: true })
+  @Prop({ minlength: 6 })
+  password?: string;
+  @Prop({ required: true, minlength: 3 })
   name: string;
   @Prop({ default: true})
   isActive: boolean;
